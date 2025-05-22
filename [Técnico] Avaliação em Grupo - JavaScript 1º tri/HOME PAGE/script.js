90827a9e8cb86ead08db8ceae6244a6f3d3cd8f2
Pedro Farenzena <pedrofm2008@gmail.com>
2025-05-21 23:06:57 -0300

    first commit

---
+++ [Técnico] Avaliação em Grupo - JavaScript 1º tri/HOME PAGE/script.js
@@ -0,0 +1,64 @@
+function cadastrarProjeto() {
+  let nomeProjeto = document.getElementById("nomeProjeto").value;
+  let descricaoProjeto = document.getElementById("descricaoProjeto").value;
+  let chaveProjeto = document.getElementById("chaveProjeto").value;
+  let gerenteProjeto = document.getElementById("gerenteProjeto").value;
+  let statusProjeto = document.getElementById("statusProjeto").value;
+  let dataInicio = document.getElementById("dataInicio").value;
+  let dataTermino = document.getElementById("dataTermino").value;
+  let arquivoProjeto = document.getElementById("arquivoProjeto").files[0];
+
+  // Validação dos campos
+  if (
+    !nomeProjeto ||
+    !descricaoProjeto ||
+    // !chaveProjeto ||
+    !gerenteProjeto ||
+    !statusProjeto ||
+    !dataInicio ||
+    !dataTermino
+  ) {
+    alert("Por favor, preencha todos os campos obrigatórios.");
+    return;
+  }
+
+  let nomeArquivo = "Nenhum arquivo enviado";
+  if (!arquivoProjeto) {
+    // ! inverte o valor e torna obrigatório o envio de um arquivo
+    nomeArquivo = arquivoProjeto.name;
+  }
+
+  let projeto = {
+    nomeProjeto,
+    descricaoProjeto,
+    //  chaveProjeto,
+    gerenteProjeto,
+    statusProjeto,
+    dataInicio,
+    dataTermino,
+    nomeArquivo,
+  };
+
+  alert(
+    `Projeto: ${projeto.nomeProjeto}\n` +
+      `Descrição: ${projeto.descricaoProjeto}\n` +
+      //`Chave: ${projeto.chaveProjeto}\n` +
+      `Gerente: ${projeto.gerenteProjeto}\n` +
+      `Status: ${projeto.statusProjeto}\n` +
+      `Data de Início: ${projeto.dataInicio}\n` +
+      `Data de Término: ${projeto.dataTermino}\n` +
+      `Arquivo: ${projeto.nomeArquivo}`
+  );
+}
+
+let chavesUnicas = [];
+
+function verificarChave(chaveProjeto) {
+  if (chavesUnicas.includes(chaveProjeto)) {
+    alert("Chave já cadastrada!");
+    return false;
+  } else {
+    chavesUnicas.push(chaveProjeto);
+    return true;
+  }
+}
